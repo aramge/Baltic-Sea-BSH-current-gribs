ca04b57ef96464c1ca0521d69379beca1637ac3c
Axel Ramge <axel@ramge.de>
2023-07-04 07:41:56 +0200

    Fixed typos

--- get-current-gribs-bsh.sh
+++ get-current-gribs-bsh.sh
@@ -11,11 +11,11 @@ where the arguments are:
 
 'ba'  Baltic Sea including Kattegat and parts of Skagerrak                  ca.   2MB in grib2-format
 'wb'  Western Baltic including Belts and Sound extending east to Bornholm   ca.   5MB in grib2-format
-'kbu' Kiel Bight                     ca. 9.8∞-11.3∞E   53.9∞-54.75∞N        ca. 0.4MB in grib2-format
-'mbu' Lübeck Bight                   ca. 10.75∞-12.5∞E 53.9∞-54.75∞N        ca. 0.3MB in grib2-format
-'rgn' Area around the isle of Rügen  ca. 12.5∞-14.5∞E  53∞-55∞N             ca. 0.8MB in grib2-format
-'snd' the Sound                      ca. 12.2∞-13.1∞E  56.1∞-56.3∞N         ca. 0.1MB in grib2-format
-'blt' the Belts                      ca. 9.5∞-11.3∞E   54.7∞-56∞N           ca.   1MB in grib2-format
+'kbu' Kiel Bight                     ca. 9.8°-11.3°E   53.9°-54.75°N        ca. 0.4MB in grib2-format
+'mbu' Lübeck Bight                   ca. 10.75°-12.5°E 53.9°-54.75°N        ca. 0.3MB in grib2-format
+'rgn' Area around the isle of Rügen  ca. 12.5°-14.5°E  53°-55°N             ca. 0.8MB in grib2-format
+'snd' the Sound                      ca. 12.2°-13.1°E  56.1°-56.3°N         ca. 0.1MB in grib2-format
+'blt' the Belts                      ca. 9.5°-11.3°E   54.7°-56°N           ca.   1MB in grib2-format
 EOF
 exit 1
 fi
